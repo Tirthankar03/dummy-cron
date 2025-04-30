2fe881d4a0a12f807d1d199712c64479a1df0822
Tirthankar03 <tirthankar.nath2@gmail.com>
2025-04-30 20:31:43 +0530

    updated cron job

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,16 @@ import cron from "node-cron";
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello Hono from dummy backend!')
 })
 
 
 // Create cron job to ping root route every 30 seconds
 cron.schedule("*/30 * * * * *", async () => {
   try {
-    await fetch(`${process.env["PROD_URL"]}/`);
-    console.log("Cron job ping successful");
+    const response = await fetch(`${process.env["CRON_URL"]}/`);
+    const text = await response.text();
+    console.log("Cron job ping successful:", text);
   } catch (error) {
     console.error("Error in cron job ping:", error);
   }
